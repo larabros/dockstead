d1c2fc99f5879a9f1e5ae0c68699d1116b7fa1c0
Hassan Khan <contact@hassankhan.me>
2016-04-24 00:28:38 +0100

    `dockstead make` now copies remaining Docker files over to local project

--- src/MakeCommand.php
+++ src/MakeCommand.php
@@ -67,6 +67,25 @@ class MakeCommand extends Command
             copy(__DIR__.'/stubs/docker-compose.yml', $this->dockerComposePath);
         }
 
+        // If ``docker`` folder does not exist
+        $dockerPath = $this->basePath.'/docker';
+        if (! file_exists($dockerPath)) {
+            mkdir($dockerPath);
+        }
+
+        // If ``docker/nginx`` folder does not exist
+        if (! file_exists($dockerPath.'/nginx')) {
+            mkdir($dockerPath.'/nginx');
+            copy(__DIR__.'/stubs/nginx/default.conf', $dockerPath.'/nginx/default.conf');
+            copy(__DIR__.'/stubs/nginx/Dockerfile', $dockerPath.'/nginx/Dockerfile');
+        }
+
+        // If ``docker/php`` folder does not exist
+        if (! file_exists($dockerPath.'/php')) {
+            mkdir($dockerPath.'/php');
+            copy(__DIR__.'/stubs/php/Dockerfile', $dockerPath.'/php/Dockerfile');
+        }
+
         $output->writeln('Dockstead Installed!');
     }
 }
