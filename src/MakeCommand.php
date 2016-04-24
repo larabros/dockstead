9f31a762def4efe10347e7e8060787ae37b076b5
Hassan Khan <contact@hassankhan.me>
2016-04-25 00:10:37 +0100

    Updated PHP Dockerfile to install GD and mbstring. Removed unnecessary import from `MakeCommand`.

--- src/MakeCommand.php
+++ src/MakeCommand.php
@@ -3,7 +3,6 @@
 namespace Larabros\Dockstead;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -56,8 +55,8 @@ class MakeCommand extends Command
     /**
      * Execute the command.
      *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  InputInterface  $input
+     * @param  OutputInterface  $output
      * @return void
      */
     public function execute(InputInterface $input, OutputInterface $output)
